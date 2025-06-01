2282a4be78e9b6e595eb3dcdf2dc12be719a9fc2
Rohejul Islam <islamrohijulr@gmail.com>
2025-06-01 12:30:19 +0600

    Create hello world SwiftUI app with default template

---
+++ CashFlow Finder/CashFlow_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashFlow_FinderApp.swift
+//  CashFlow Finder
+//
+//  Created by Md. Rohejul Islam on 6/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CashFlow_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
