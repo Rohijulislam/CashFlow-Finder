9d7fdf216fc3c00758f328fe5c4a39ea6c84b318
Rohejul Islam <islamrohijulr@gmail.com>
2025-06-01 14:47:37 +0600

    Setup root screen with empty state view

--- CashFlowFinderLibrary/Sources/AppFeature/AppFeature.swift
+++ CashFlowFinderLibrary/Sources/AppFeature/AppFeature.swift
@@ -1,2 +1,15 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+import Shared
+
+public struct RootView: View {
+    public init() {
+        // Initialization code if needed
+    }
+    
+    public var body: some View {
+        EmptyStateView(showPicker: .constant(true))
+    }
+}
