b72ba098f8c9068fd33d56003f6da39ef60d6504
Rohejul Islam <islamrohijulr@gmail.com>
2025-06-01 14:13:03 +0600

    Create  CashFlowFinderLibrary package to support multi module architecture

---
+++ CashFlowFinderLibrary/Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CashFlowFinderLibrary",
+    defaultLocalization: "en",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(name: "AppFeature", targets: ["AppFeature"]),
+        .library(name: "Shared", targets: ["Shared"]),
+        .library(name: "Utils", targets: ["Utils"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "AppFeature"),
+        .target(
+            name: "Shared"),
+        .target(
+            name: "Utils"),
+    ]
+)
---
+++ CashFlowFinderLibrary/Sources/Utils/Utils.swift
@@ -0,0 +1,8 @@
+//
+// Utils.swift
+//  CashFlowFinderLibrary
+//
+//  Created by Md. Rohejul Islam on 6/1/25.
+//
+
+import Foundation
